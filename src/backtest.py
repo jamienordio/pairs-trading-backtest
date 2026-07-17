3d866893ffb631d7f638dc7f3e2e99ac532bbaf6
Jamie Nordio <nordio.jamie@gmail.com>
2026-07-16 19:30:57 -0700

    updated modules with new methods
    - spread.py: compute_rolling_zscore, compute_rolling_spread
    - backtest.py: compute_strategy_returns_dynamic_beta
    - notebook 01: added rolling windows to spread, zscore, beta calculations. compared with full data calculations. ran analysis on different tickers.

--- src/backtest.py
+++ src/backtest.py
@@ -46,3 +46,49 @@ def compute_strategy_returns(
     strategy_returns.name = f"{ticker_y}_{ticker_x}_strategy_returns"
 
     return strategy_returns
+
+def compute_strategy_returns_dynamic_beta(
+    prices: pd.DataFrame,
+    positions: pd.Series,
+    beta: pd.Series,
+    ticker_y: str,
+    ticker_x: str,
+    transaction_cost_bps: float = 0.0,
+) -> pd.Series:
+    """
+    Compute daily strategy returns using time-varying hedge ratio beta_t
+
+    Positions
+    ---------
+     1 = long spread
+    -1 = short spread
+     0 = flat
+    """
+    y = prices[ticker_y]
+    x = prices[ticker_x]
+    
+    beta_lagged = beta.shift(1) # previous deta beta
+    long_spread_pnl = y.diff() - beta_lagged*x.diff() # long-spread P&L
+    gross_exposure = y.shift(1).abs() + beta_lagged.abs()*x.shift(1).abs()
+    lagged_positions = positions.shift(1).fillna(0) # lag positions by 1 day
+    turnover = positions.diff().abs().shift(1).fillna(0)
+
+    df = pd.concat(
+        {
+            "position": lagged_positions,
+            "long_spread_pnl": long_spread_pnl,
+            "gross_exposure": gross_exposure,
+            "turnover": turnover,
+        },
+        axis=1,
+    ).dropna()
+
+    cost_per_turnover = transaction_cost_bps / 10_000
+
+    strategy_returns = (
+        df["position"] * df["long_spread_pnl"] / df["gross_exposure"]
+        - cost_per_turnover * df["turnover"]
+    )
+
+    strategy_returns.name = f"{ticker_y}_{ticker_x}_dynamic_beta_strategy_returns"
+    return strategy_returns
